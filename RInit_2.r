3625540225cdc3dafcb16dc857eb1f9ddd3f18ee
Vicken-Ghoubiguian <ericghoubiguian@live.fr>
2020-12-28 14:41:54 +0000

    Encapsulation of all the previous code in a function...

--- RInit_2.r
+++ RInit_2.r
@@ -1,17 +1,20 @@
-# Installation of "httr" (for HTTP requests), "jsonlite" (for JSON management) and "magick" (for image processing) packages...
-install.packages(c("httr", "jsonlite", "magick"))
+# Definition of the function to get, extract and return all GitHub emojis...
+get_all_GitHub_emojis <- function(){
 
-# Importation of both newly installed packages...
-library(httr)
-library(jsonlite)
-library(magick)
-
-# Creation of HTTP GitHub User request string and affectation to 'httpGetAllGitHubEmojisRequest' variable...
-httpGetAllGitHubEmojisRequest <- paste("https://api.github.com/emojis")
-
-# 
-res = GET(httpGetAllGitHubEmojisRequest)
-
-# Display the result in the console...
-print(res)
-
+  # Installation of "httr" (for HTTP requests), "jsonlite" (for JSON management) and "magick" (for image processing) packages...
+  install.packages(c("httr", "jsonlite", "magick"))
+  
+  # Importation of both newly installed packages...
+  library(httr)
+  library(jsonlite)
+  library(magick)
+  
+  # Creation of HTTP GitHub User request string and affectation to 'httpGetAllGitHubEmojisRequest' variable...
+  httpGetAllGitHubEmojisRequest <- paste("https://api.github.com/emojis")
+  
+  # 
+  res = GET(httpGetAllGitHubEmojisRequest)
+  
+  # Display the result in the console...
+  print(res)
+}
