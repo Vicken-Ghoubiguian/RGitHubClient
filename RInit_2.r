6fb3fed9d41db6059abb90b76260e857f877efb1
Vicken-Ghoubiguian <ericghoubiguian@live.fr>
2021-02-09 17:28:06 +0000

    The return of the current king (or the moment king)...

--- RInit_2.r
+++ RInit_2.r
@@ -1,13 +1,13 @@
-# Importation of both newly installed packages...
-library(httr)
-library(jsonlite)
-library(magick)
-
-# Installation of "httr" (for HTTP requests), "jsonlite" (for JSON management) and "magick" (for image processing) packages...
-install.packages(c("httr", "jsonlite", "magick"))
-
 # Definition of the function to get, extract and return all GitHub emojis...
 get_all_GitHub_emojis <- function(){
+  
+  # Installation of "httr" (for HTTP requests), "jsonlite" (for JSON management) and "magick" (for image processing) packages...
+  install.packages(c("httr", "jsonlite", "magick"))
+  
+  # Importation of both newly installed packages...
+  library(httr)
+  library(jsonlite)
+  library(magick)
   
   # Creation of HTTP GitHub User request string and affectation to 'httpGetAllGitHubEmojisRequest' variable...
   httpGetAllGitHubEmojisRequest <- paste("https://api.github.com/emojis")
